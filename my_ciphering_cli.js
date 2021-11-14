8ce5ce9e6ff324df8a95688f49df3d3c45d75cf5
Akphillips <phillipjohnson7889@mail.ru>
2021-11-15 01:46:42 +0500

    feat: main script, parse object and main file

---
+++ my_ciphering_cli.js
@@ -0,0 +1,121 @@
+const fs = require('fs');
+const path = require('path');
+const program = require('./index');
+const stream = require('stream'); 
+
+
+const { config, input, output } = program.optionValues;
+const { Writable } = require('stream');
+const myStream = new Writable();
+const fooErr = new Error('foo error');
+myStream.destroy(fooErr)
+myStream.on('error', (fooErr) => console.error(fooErr.message))
+console.log(myStream)
+
+// const readStream = fs.createReadStream(path.join(__dirname, input));
+
+// readStream.on
+// const myReadStream = input ? fs.createReadStream(path.join(__dirname, input)) : process.stdin;
+// const myWriteStream = output ? fs.createWriteStream(path.join(__dirname, output), )
+// const stream = require('stream');
+// const util = require('util');
+// const cisarShift = require('./cisar_shipher');
+
+// const Transform = stream.Transform || require('readable-stream').Transform;
+
+// function Cesar(options) {
+
+//     if (!(this instanceof Cesar)) {
+//         return new Cesar(options);
+//     }
+
+//     Transform.call(this, options);
+// }
+// util.inherits(Cesar, Transform);
+
+// Cesar.prototype._transform = function (chunk, enc, cb) {
+//     const cesarChunk = cisarShift(chunk.toString(), 1);
+//     this.push(cesarChunk);
+//     cb();
+// }
+
+
+// const cesar = new Cesar();
+// cesar.pipe(process.stdout);
+// cesar.write('hello world');
+// cesar.end();
+// var stream = require('stream');
+// var util = require('util');
+
+// // node v0.10+ use native Transform, else polyfill
+// var Transform = stream.Transform ||
+//   require('readable-stream').Transform;
+
+// /*
+//  * Filters an object stream properties
+//  *
+//  * @param filterProps array of props to filter
+//  */
+// function Filter(filterProps, options) {
+//   // allow use without new
+//   if (!(this instanceof Filter)) {
+//     return new Filter(filterProps, options);
+//   }
+
+//   // init Transform
+//   if (!options) options = {}; // ensure object
+//   options.objectMode = true; // forcing object mode
+//   Transform.call(this, options);
+//   this.filterProps = filterProps;
+// }
+// util.inherits(Filter, Transform);
+
+// /* filter each object's sensitive properties */
+// Filter.prototype._transform = function (obj, enc, cb) {
+//   var self = this;
+//   // determine what keys to keep
+//   var filteredKeys = Object.keys(obj).filter(
+//     function (key) {
+//       // only those keys not in this list
+//       return (self.filterProps.indexOf(key) === -1);
+//     }
+//   );
+
+//   // create clone with only these keys
+//   var filteredObj = filteredKeys.reduce(
+//     function (accum, key) {
+//       accum[key] = obj[key];
+//       return accum;
+//     },
+//     {}
+//   );
+
+//   // push the filtered obj out
+//   this.push(filteredObj);
+//   cb();
+// };
+
+
+// // try it out, output to stdout
+// // filter phone and email from objects
+// var filter = new Filter([ 'phone', 'email' ]);
+// filter
+//   .on('readable', function () {
+//     var obj;
+//     while (null !== (obj = filter.read())) {
+//       console.log(obj);
+//     }
+//   });
+
+// // now send some objects to filter through
+// filter.write({ name: 'Foo', phone: '555-1212',
+//                email: 'foo@foo.com', id: 123 });
+// filter.write({ name: 'Bar', phone: '555-1313',
+//                email: 'bar@bar.com', id: 456 });
+// filter.end();  // finish
+
+// const { createReadStream, createWriteStream } = require('fs');
+// // const readStream = createReadStream('./anime_dancing.mp4');
+// const writeStream = createWriteStream('./file.txt');
+
+// process.stdin.pipe(writeStream);
